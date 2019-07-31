97240afcd814922b9d8757020a05bc461026b417
quizeo <53500165+quizeo@users.noreply.github.com>
2019-07-31 09:32:57 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,50 @@
+<!DOCTYPE html>
 <html>
-<head><title>hello world<title>
+<style>
+body {
+	background-color:" lightblue";
+}
+
+h1 {
+  color: white;
+  text-align: left;
+  font-family: Tekton Pro Ext;\
+
+}
+h3 {
+	font-family: Goudy Stout;
+
+
+}
+p1 {
+  color: blue;
+  font-family: bold;
+  font-size: 30px ;
+  
+
+p {
+  font-family: verdana;
+  font-size: 20px;
+}
+</style>
+<head>
+	<title>Product</title>
+</head>
+<body>
+<hr style="background-color:darkgray; border:thin ;" width="1500 " size="60">
+<center><h1 style="background-color: darkgray"> Zeophone</h1></center>
+<hr>
+ <img src="phone.jpg" align="left">
+ <h2 style="background-color:lightblue">VIVO V11</h2>
+ <p1>P18,420</p1>
+ <p>Body: Glass body, plastic frame 157.9 x 75 x 7.9 mm, 156gScreen: 6.41" 2,340 x 1,080px (19.5:9) Super AMOLED, 402ppi, notched</p>
+ <p>Chipset: Snapdragon 660 chipset - octa-core processor (4x2.2 GHz Kryo 260 & 4x1.8 GHz Kryo 260), Adreno 512 GPU</p>
+<p>Memory: 6GB RAM, 128GB of storage, (dedicated) microSD slot</p>
+<p>Camera: Dual: 12MP (f/1.8) + 5MP for depth sensing, phase detection autofocus; 1080p @ 30fps video</p>
+<p>Battery: 3,400 mAh, 18W fast charging</p>
+
+
+
+
+</body>
+</html>
